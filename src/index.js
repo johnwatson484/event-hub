eb85c61ddec9d086038588e96c7e5bc0e724bc43
John Watson <john.watson1@defra.gov.uk>
2025-02-02 12:25:01 +0000

    Add initial SNS/SQS setup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,54 @@
-import 'log-timestamp'
+import { PublishCommand, SNSClient } from '@aws-sdk/client-sns'
+import { ReceiveMessageCommand, DeleteMessageBatchCommand, SQSClient } from '@aws-sdk/client-sqs'
 
-console.log('Hello, world!')
+const TOPIC_ARN = process.env.AWS_SNS_TOPIC_ARN
+const QUEUE_URL = process.env.AWS_SQS_QUEUE_URL
+
+const credentials = {
+  accessKeyId: process.env.ACCESS_KEY,
+  secretAccessKey: process.env.SECRET_KEY,
+}
+
+async function sendMessage (message) {
+  const client = new SNSClient({ region: 'eu-west-2', credentials })
+  await client.send(
+    new PublishCommand({
+      Message: message,
+      TopicArn: TOPIC_ARN,
+      MessageGroupId: 'demo-group',
+      MessageDeduplicationId: Date.now().toString(),
+    })
+  )
+  console.log('Message sent')
+}
+
+async function receiveMessage () {
+  const client = new SQSClient({ region: 'eu-west-2', credentials })
+  const { Messages } = await client.send(
+    new ReceiveMessageCommand({
+      QueueUrl: QUEUE_URL,
+      MaxNumberOfMessages: 10,
+      MessageAttributeNames: ['All'],
+      AttributeNames: ['SentTimestamp'],
+      WaitTimeSeconds: 10,
+    })
+  )
+  if (Messages) {
+    console.log(`Messages received: ${Messages.length}`)
+    Messages.forEach((message) => {
+      console.log(JSON.parse(message.Body).Message)
+    })
+    await client.send(
+      new DeleteMessageBatchCommand({
+        QueueUrl: QUEUE_URL,
+        Entries: Messages.map((message) => ({
+          Id: message.MessageId,
+          ReceiptHandle: message.ReceiptHandle,
+        })),
+      })
+    )
+  }
+}
+
+await sendMessage('Hello, world!')
+await receiveMessage()
